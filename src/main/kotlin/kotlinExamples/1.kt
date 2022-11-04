bc5f4165a2db1f8658e9be7c6e19a60ceb1a525a
oybekjon94 <x.oybek94@gmail.com>
2022-11-05 04:10:46 +0900

    [feat::] Add 1 exercise

--- src/main/kotlin/kotlinExamples/1.kt
+++ src/main/kotlin/kotlinExamples/1.kt
@@ -1,2 +1,20 @@
 package kotlinExamples
 
+import java.util.*
+
+/*
+How to Print an Integer entered
+by an user in Kotlin using Scanner
+ */
+fun main(){
+    // Creates a reader instance which takes
+    // input from standard input - keyboard
+    val reader = Scanner(System.`in`)
+    print("Enter a number: ")
+
+    // nextInt() reads the next integer from the keyboard
+    var integer:Int = reader.nextInt()
+
+    // println() prints the following line to the output screen
+    println("You entered: $integer")
+}
