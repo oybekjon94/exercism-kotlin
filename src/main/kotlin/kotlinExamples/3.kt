5a0ee4a649ccc1156f772a43cc15657a20f562a2
oybekjon94 <x.oybek94@gmail.com>
2022-11-05 07:02:28 +0900

    [feat::] Add 3 exercise

---
+++ src/main/kotlin/kotlinExamples/3.kt
@@ -0,0 +1,10 @@
+package kotlinExamples
+
+//multiply two floating point numbers
+fun main(){
+    val first = 1.5f
+    val second = 2.0f
+
+    val product = first * second
+    println("the product is $product")
+}
