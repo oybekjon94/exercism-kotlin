9a0f8478d94687245a2949b142b8ac4e08e70d66
oybekjon94 <x.oybek94@gmail.com>
2022-11-05 06:57:52 +0900

    [feat::] Add 2 exercise

---
+++ src/main/kotlin/kotlinExamples/2.kt
@@ -0,0 +1,10 @@
+package kotlinExamples
+
+// kotlin program to add two integers
+fun main(){
+    val first: Int = 10
+    val second: Int = 20
+
+    val sum  = first + second
+    println(sum)
+}
