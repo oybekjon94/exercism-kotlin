8d556d8d476239c00d0f77df52bcc7ea119b6afc
oybekjon94 <x.oybek94@gmail.com>
2022-11-08 17:40:47 +0900

    [feat::] Add 43 exercise

---
+++ src/main/kotlin/kotlinExamples/43.kt
@@ -0,0 +1,21 @@
+package kotlinExamples
+
+import java.util.Calendar
+
+// Add Two Dates
+fun main(){
+    val c1 = Calendar.getInstance()
+    val c2 = Calendar.getInstance()
+    val cTotal = c1.clone() as Calendar
+
+    cTotal.add(Calendar.YEAR, c2.get(Calendar.YEAR))
+    cTotal.add(Calendar.MONTH, c2.get(Calendar.MONTH) +1) //zero-based months
+    cTotal.add(Calendar.DATE, c2.get(Calendar.DATE))
+    cTotal.add(Calendar.HOUR_OF_DAY, c2.get(Calendar.HOUR_OF_DAY))
+    cTotal.add(Calendar.MINUTE, c2.get(Calendar.MINUTE))
+    cTotal.add(Calendar.SECOND, c2.get(Calendar.SECOND))
+    cTotal.add(Calendar.MILLISECOND, c2.get(Calendar.MILLISECOND))
+
+    println("${c1.time} + ${c2.time} = ${cTotal.time}")
+
+}
