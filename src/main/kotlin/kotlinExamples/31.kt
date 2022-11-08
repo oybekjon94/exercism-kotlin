23dbf86fad96b6e0a6162790cfd7de962e50d161
oybekjon94 <x.oybek94@gmail.com>
2022-11-08 10:25:28 +0900

    [feat::] Add 31 exercise

---
+++ src/main/kotlin/kotlinExamples/31.kt
@@ -0,0 +1,10 @@
+package kotlinExamples
+
+import java.util.Arrays
+
+//Print an Array using standard library Arrays
+fun main(){
+    val array = intArrayOf(1,2,3,4,5)
+
+    println(Arrays.toString(array))
+}
