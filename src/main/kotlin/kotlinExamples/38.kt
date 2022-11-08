0511bf76edfa6f1a24339e772be4e9f405d07921
oybekjon94 <x.oybek94@gmail.com>
2022-11-08 14:35:55 +0900

    [feat::] Add 38 exercise

---
+++ src/main/kotlin/kotlinExamples/38.kt
@@ -0,0 +1,11 @@
+package kotlinExamples
+
+import java.util.*
+
+//Convert String to char array
+fun main(){
+    val st = "this is great"
+
+    val chars = st.toCharArray()
+    println(Arrays.toString(chars))
+}
