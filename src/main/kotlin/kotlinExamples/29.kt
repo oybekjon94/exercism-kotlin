09b02df25f3b6594d20c4368f28d5eae0671d558
oybekjon94 <x.oybek94@gmail.com>
2022-11-08 10:15:00 +0900

    [feat::] Add 29 exercise

---
+++ src/main/kotlin/kotlinExamples/29.kt
@@ -0,0 +1,13 @@
+package kotlinExamples
+
+//Factors of a Positive Integer
+fun main(){
+    val number = 60
+
+    print("factors of $number are : " )
+    for (i in 1..number){
+        if (number % i == 0){
+            print("$i ")
+        }
+    }
+}
