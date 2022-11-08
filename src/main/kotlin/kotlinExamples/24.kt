3a1fe90c409b1dbac1e9f662776606b920b92d04
oybekjon94 <x.oybek94@gmail.com>
2022-11-08 09:46:16 +0900

    [feat::] Add 24 exercise

---
+++ src/main/kotlin/kotlinExamples/24.kt
@@ -0,0 +1,13 @@
+package kotlinExamples
+
+//Display Characters from A to Z using loop
+//Display Uppercased A to Z
+fun  main(){
+    var c: Char
+
+    c = 'A'
+    while (c <= 'Z'){
+        print("$c")
+        ++c
+    }
+}
