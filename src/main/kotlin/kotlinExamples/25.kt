b4bfcb8917ab4f817ace867dfbf589689ca53118
oybekjon94 <x.oybek94@gmail.com>
2022-11-08 09:48:38 +0900

    [feat::] Add 25 exercise

---
+++ src/main/kotlin/kotlinExamples/25.kt
@@ -0,0 +1,12 @@
+package kotlinExamples
+
+//Display Characters from A to Z using loop
+//Display Lowercased a to z
+fun main(){
+    var c: Char
+    c='a'
+    while (c <= 'z'){
+        print("$c")
+        ++c
+    }
+}
