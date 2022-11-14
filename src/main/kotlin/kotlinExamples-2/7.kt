858893ec0623a1cf4066578df7503c3773877140
oybekjon94 <x.oybek94@gmail.com>
2022-11-14 19:08:26 +0900

    [feat::] Add 7 exercise

---
+++ src/main/kotlin/kotlinExamples-2/7.kt
@@ -0,0 +1,11 @@
+package `kotlinExamples-2`
+
+fun main(){
+    //Increment & Decrement operators(++, --)
+    var myNum = 5
+    myNum ++
+    println("myNum is $myNum")
+    println("myNum is ${myNum++}")
+    println("myNum is ${++myNum}")
+}
+
