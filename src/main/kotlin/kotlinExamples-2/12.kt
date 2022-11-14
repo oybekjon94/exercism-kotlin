8d470e6dbb12cad369176034795d188ddd8542dc
oybekjon94 <x.oybek94@gmail.com>
2022-11-14 20:04:02 +0900

    [feat::] Add 12 exercise

---
+++ src/main/kotlin/kotlinExamples-2/12.kt
@@ -0,0 +1,24 @@
+package `kotlinExamples-2`
+
+fun main(){
+    var season = 3
+    when(season){
+        1 -> println("Spring")
+        2 -> println("Summer")
+        3 -> {
+            println("Fall")
+            println("Autumn")
+        }
+        4-> println("Winter")
+        else -> println("Invalid season")
+
+    }
+    var month = 3
+    when(month){
+        in 3..5 -> println("Spring")
+        in 6..8 -> println("Summer")
+        in 9..11 -> println("Fall")
+        12, 1, 2 -> println("Winter")
+        else -> println("Invalid season")
+    }
+}
