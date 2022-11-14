57182885245b82d35a54ddd503fe6b67ba10fb43
oybekjon94 <x.oybek94@gmail.com>
2022-11-14 17:26:11 +0900

    [feat::] Add 4 exercise

---
+++ src/main/kotlin/kotlinExamples-2/4.kt
@@ -0,0 +1,12 @@
+package `kotlinExamples-2`
+
+//arifmetic operators (+,-,*,/,%)
+fun main(){
+    var result = 5+20
+    val a = 5.0
+    val b = 3
+    var resultDouble : Double
+    resultDouble = a/b
+
+    print(resultDouble)
+}
