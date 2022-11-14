d6d92477c67cd8cd148c8f4231169a561259aad8
oybekjon94 <x.oybek94@gmail.com>
2022-11-14 18:57:46 +0900

    [feat::] Add 5 exercise

---
+++ src/main/kotlin/kotlinExamples-2/5.kt
@@ -0,0 +1,14 @@
+package `kotlinExamples-2`
+
+fun main(){
+    //comparison operators(==, !=, <, >, <=, >=)
+    val isEqual = 5==3
+    println("isEqual is $isEqual")
+
+    val isNOtEqual = 5!=5
+    println("isNotEqual is $isNOtEqual")
+
+    println("is 5 greater 3 ${5>3}")
+    println("is 5 lower Equal 3  ${5 <= 3}")
+}
+
