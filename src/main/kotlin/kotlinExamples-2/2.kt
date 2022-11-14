e2af9225b7b281a0bd8d0fddff644c1875e0cbf4
oybekjon94 <x.oybek94@gmail.com>
2022-11-14 17:14:28 +0900

    [feat::] Add 2 exercise

---
+++ src/main/kotlin/kotlinExamples-2/2.kt
@@ -0,0 +1,11 @@
+package `kotlinExamples-2`
+
+fun main(){
+    //String
+    val myStr = "Hello world"
+    var firstCharInStr = myStr[0]
+    var lastCharInStr = myStr[myStr.length -1]
+    var mylength = myStr.length
+
+    println("First character $firstCharInStr and the length of myStr is ${myStr.length}")
+}
