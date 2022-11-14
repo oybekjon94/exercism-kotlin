d33513bb27503785746efe53259c3269dfea9d8d
oybekjon94 <x.oybek94@gmail.com>
2022-11-14 19:30:48 +0900

    [feat::] Add 10 exercise

---
+++ src/main/kotlin/kotlinExamples-2/10.kt
@@ -0,0 +1,11 @@
+package `kotlinExamples-2`
+
+fun main(){
+    var name = "Denis"
+
+    if (name == "Denis"){
+        println("welcome home Denis")
+    }else{
+        println("who are you?")
+    }
+}
