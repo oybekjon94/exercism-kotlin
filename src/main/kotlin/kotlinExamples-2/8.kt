565f449c665abfe8ffab0f88ac550b75d504ba5a
oybekjon94 <x.oybek94@gmail.com>
2022-11-14 19:17:45 +0900

    [feat::] Add 8 exercise

---
+++ src/main/kotlin/kotlinExamples-2/8.kt
@@ -0,0 +1,14 @@
+package `kotlinExamples-2`
+
+fun main(){
+    var heightPerson1 = 170
+    var heightPerson2 = 189
+
+    if (heightPerson1  > heightPerson2){
+        println("use raw force")
+    }else if (heightPerson1 == heightPerson2){
+        println("use your power technique 1337")
+    }else{
+        println("use techniques")
+    }
+}
