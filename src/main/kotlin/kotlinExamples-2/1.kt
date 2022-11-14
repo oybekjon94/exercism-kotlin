207cc19d90b33b481e4df0a2f2bb301b67796b33
oybekjon94 <x.oybek94@gmail.com>
2022-11-14 17:09:47 +0900

    [feat::] Add 1 exercise

---
+++ src/main/kotlin/kotlinExamples-2/1.kt
@@ -0,0 +1,11 @@
+package `kotlinExamples-2`
+
+fun main(){
+    //String
+    val myStr = "Hello world"
+    var firstCharInStr = myStr[0]
+    var lastCharInStr = myStr[myStr.length -1]
+
+    println("First character ${firstCharInStr}")
+    println("First character ${lastCharInStr}")
+}
