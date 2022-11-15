7f680f97612743dd18b263121841c22366efe330
oybekjon94 <x.oybek94@gmail.com>
2022-11-15 09:21:06 +0900

    [feat::] Add 15 exercise

---
+++ src/main/kotlin/kotlinExamples-2/15.kt
@@ -0,0 +1,12 @@
+package `kotlinExamples-2`
+
+//data type
+fun main(){
+    val name: String = "oybek"
+    val  age: Int = 29
+    val gender = "man"
+    val color = "white"
+    val address = "Kashkadarya region"
+
+    println("my name is $name age is $age and gender is $gender my body color is $color and my address is $address")
+}
