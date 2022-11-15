9a2a5c06e09210fe04f8e6fe8fdfe2df61dc7fc7
oybekjon94 <x.oybek94@gmail.com>
2022-11-15 12:08:39 +0900

    [feat::] Add 2 exercise

---
+++ src/main/kotlin/kotlinExamples-3/2.kt
@@ -0,0 +1,14 @@
+package `kotlinExamples-3`
+
+fun main(){
+
+    for (num in 1..10){
+        println("$num")
+    }
+    for (i in 1 until 10){
+        println("$i")
+    }
+    for (i in 10 downTo 1 step 2){
+        println("$i")
+    }
+}
