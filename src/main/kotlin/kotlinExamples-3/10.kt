e639585d6b308b0d059c3b103aec0c8ffa83943e
oybekjon94 <x.oybek94@gmail.com>
2022-11-16 14:20:19 +0900

    [feat::] Add 10 exercise

---
+++ src/main/kotlin/kotlinExamples-3/10.kt
@@ -0,0 +1,11 @@
+package `kotlinExamples-3`
+
+/*
+Kotlin provides subSequence() function to return a substring between the specified start and end index.
+ */
+// Syntax -> subSequence(startIndex, endIndex)
+//kotlin substring
+fun main(){
+    val str = "Welcome, Somya"
+    println(str.subSequence(0,6))
+}
