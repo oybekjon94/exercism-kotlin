694aada8fc1d4bd5dcc415ea07d6080043705098
oybekjon94 <x.oybek94@gmail.com>
2022-11-16 14:50:58 +0900

    [feat::] Add 11 exercise

---
+++ src/main/kotlin/kotlinExamples-3/11.kt
@@ -0,0 +1,10 @@
+package `kotlinExamples-3`
+
+//kotlin raw string
+fun main(){
+    val str = """welcome , Somya
+        you are learning 
+        koltin programming language
+    """
+    println(str)
+}
