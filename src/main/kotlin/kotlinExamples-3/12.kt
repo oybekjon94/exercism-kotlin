3d97ad1ee26eb6c58802f1d39bd1ed695da8ac1b
oybekjon94 <x.oybek94@gmail.com>
2022-11-16 14:55:01 +0900

    [feat::] Add 12 exercise

---
+++ src/main/kotlin/kotlinExamples-3/12.kt
@@ -0,0 +1,10 @@
+package `kotlinExamples-3`
+
+//kotlin plus
+fun main(){
+    val str = "welcome , Somya"
+    val str1 = " how are you"
+    var result: String
+    result = str.plus(str1)
+    println("$result")
+}
