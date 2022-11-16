bef52b6c51f52b63ab6309946126c2cd78f79a0a
oybekjon94 <x.oybek94@gmail.com>
2022-11-16 16:12:21 +0900

    [feat::] Add 15 exercise

---
+++ src/main/kotlin/kotlinExamples-3/15.kt
@@ -0,0 +1,12 @@
+package `kotlinExamples-3`
+
+fun main(){
+    val a = intArrayOf(4,5,6)
+    val c = charArrayOf('x','y','a')
+    for (x in a){
+        println(x)
+    }
+    for (y in c){
+        println(y)
+    }
+}
