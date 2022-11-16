b1d232c752c585e6c0b76d8c664ae70233637af4
oybekjon94 <x.oybek94@gmail.com>
2022-11-16 14:09:22 +0900

    [feat::] Add 7 exercise

---
+++ src/main/kotlin/kotlinExamples-3/7.kt
@@ -0,0 +1,12 @@
+package `kotlinExamples-3`
+
+//string iteration
+fun main(){
+    var str = """this is envisioned and developed
+        to meet basic and free courses to 
+        learn , succed and belong
+    """
+    for (s in str){
+        println(s)
+    }
+}
