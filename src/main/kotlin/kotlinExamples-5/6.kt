8b2f0eda75ccd2717a94f3b2a5c75dd7082a1bee
oybekjon94 <x.oybek94@gmail.com>
2022-11-18 15:29:00 +0900

    [feat::] Add 6 exercise

---
+++ src/main/kotlin/kotlinExamples-5/6.kt
@@ -0,0 +1,19 @@
+package `kotlinExamples-5`
+
+fun main(){
+    //declared a variable with a value of 10
+    var number = 10
+
+    //while:
+    //the body is executed while the condition is true
+
+    while (number>=5){
+        println("Number: $number")
+        number--
+    }
+    while (number<=10){
+        println("Number +: $number")
+        number++
+    }
+
+}
