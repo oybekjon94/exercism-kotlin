0820da5a30176f946274a867fa9eb3f0833047a6
oybekjon94 <x.oybek94@gmail.com>
2022-11-18 15:34:31 +0900

    [feat::] Add 7 exercise

---
+++ src/main/kotlin/kotlinExamples-5/7.kt
@@ -0,0 +1,14 @@
+package `kotlinExamples-5`
+
+//Fizz-Buzz game
+fun fizzBuzz(i: Int) = when{
+    i % 15 == 0 -> "Fizz Buzz"
+    i % 3 == 0 -> "Fizz"
+    i % 5 == 0 -> "Fizz Buzz"
+    else -> "$i"
+}
+fun main(){
+    for (i in 1..100){
+        println(fizzBuzz(i))
+    }
+}
