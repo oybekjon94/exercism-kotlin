5f83fac903d194a42d1444fb0cb5f4da9b461ff6
oybekjon94 <x.oybek94@gmail.com>
2022-11-18 14:42:17 +0900

    [feat::] Add 3 exercise

---
+++ src/main/kotlin/kotlinExamples-5/3.kt
@@ -0,0 +1,19 @@
+package `kotlinExamples-5`
+
+enum class Books{
+    CLASSICAL,
+    FANTASY,
+    ACTION,
+    ADVENTURE,
+    HORROR
+}
+fun compareBooks(b1: Books, b2:Books) =
+    when{
+        (b1 == Books.ACTION && b2 == Books.ADVENTURE) -> println("I prefer to read Adventure books")
+        (b1 == Books.FANTASY && b2 == Books.HORROR) -> println("I prefer to read Fantasy books")
+        (b1 == Books.CLASSICAL && b2 == Books.FANTASY) -> println("I prefer to read Classical books")
+        else -> throw Exception("Unkwon")
+    }
+fun main(){
+    compareBooks(Books.ACTION, Books.ADVENTURE)
+}
