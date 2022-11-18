fb297e2270f4a15ff290302eca73a7e704a60cba
oybekjon94 <x.oybek94@gmail.com>
2022-11-18 14:20:20 +0900

    [feat::] Add 16 exercise

---
+++ src/main/kotlin/kotlinExamples-5/1.kt
@@ -0,0 +1,20 @@
+package `kotlinExamples-5`
+
+enum class Seasons{
+    SPRING,
+    SUMMER,
+    AUTUMN,
+    WINTER
+}
+
+fun getSeason(seasons: Seasons) =
+    when(seasons){
+        Seasons.SPRING, Seasons.SUMMER -> println("warm and hot")
+        Seasons.AUTUMN-> println("cold")
+        Seasons.WINTER -> println("too cold")
+    }
+
+fun main(){
+    getSeason(Seasons.SPRING)
+    getSeason(Seasons.SUMMER)
+}
