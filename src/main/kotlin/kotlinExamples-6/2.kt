7d89b944218f9108928d02446365a12b0f19151c
oybekjon94 <x.oybek94@gmail.com>
2022-11-18 21:14:13 +0900

    [feat::] Add 2 exercise

---
+++ src/main/kotlin/kotlinExamples-6/2.kt
@@ -0,0 +1,23 @@
+package `kotlinExamples-6`
+
+data class User(val id: Long, var name: String)
+
+fun main(){
+    val user1 = User(1,"oybek")
+
+    val name = user1.name
+    println(name)
+    user1.name = "michael"
+    val user2 = User(1,"michael")
+
+    println(user1 == user2)
+
+    println("User Details: $user1")
+
+    val updatedUser = user1.copy(name="Oybek Kholikov")
+    println(user1)
+    println(updatedUser)
+
+    println(updatedUser.component1()) // print
+    println(updatedUser.component2()) // prints Denis Panjuta
+}
