9c36228d39967ffa29c60872f20bf52e69424975
oybekjon94 <x.oybek94@gmail.com>
2022-11-22 16:39:27 +0900

    [feat::] Add 11 exercise

---
+++ src/main/kotlin/kotlinExamples-7/11.kt
@@ -0,0 +1,23 @@
+package `kotlinExamples-7`
+
+// By default,
+// public final class View
+
+// With 'open' keyword:
+// public class View
+open class View{
+    open fun click() = println("View clicked")
+}
+class Button: View(){
+    override fun click(){
+        println("Button clicked")
+    }
+}
+fun View.show() = println("View shows")
+fun Button.show() = println("Button shows")
+
+fun main(){
+    val view: View = Button()
+    view.click()
+    view.show()
+}
