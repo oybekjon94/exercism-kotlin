3a921faa53862dc8af7c7c07c9268ee23bbf9d65
oybekjon94 <x.oybek94@gmail.com>
2022-11-22 16:20:00 +0900

    [feat::] Add 8 exercise

---
+++ src/main/kotlin/kotlinExamples-7/8.kt
@@ -0,0 +1,24 @@
+package `kotlinExamples-7`
+
+import java.util.Collections
+
+
+fun <T> joinToString(
+    collection: Collection<T>,
+    separator: String,
+    prefix: String,
+    postFix: String
+): String{
+    val result = StringBuilder(prefix)
+
+    for ((index, element) in collection.withIndex()){
+        if (index>0) result.append(separator)
+        result.append(element)
+    }
+    result.append(postFix)
+    return result.toString()
+}
+fun main(){
+    val list = listOf(1,2,3)
+    println(joinToString(list,";","(",")" ))
+}
