fb66172e29e3e9481d5ab46cb06d1e664f30d0e4
oybekjon94 <x.oybek94@gmail.com>
2022-11-24 14:12:11 +0900

    [feat::] Add 1 exercise

---
+++ src/main/kotlin/kotlinExamples-8/1.kt
@@ -0,0 +1,9 @@
+package `kotlinExamples-8`
+
+fun main(){
+    val string = "kotlin"
+    val string2 = "kotlin"
+    println(string == string2)
+    println(string.equals(string2))
+    println(string === string2)
+}
