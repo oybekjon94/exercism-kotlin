fb8ac822a889345b41c1feadd4e2dfefa7d7607d
oybekjon94 <x.oybek94@gmail.com>
2022-11-24 14:32:19 +0900

    [feat::] Add 3 exercise

---
+++ src/main/kotlin/kotlinExamples-8/3.kt
@@ -0,0 +1,56 @@
+package `kotlinExamples-8`
+
+
+//this example about cooperating functions
+fun main(){
+    functionA(1)
+}
+
+fun functionA(case: Int){
+    when(case){
+        //here , function will do the taskA1 and
+        // give control to the function to execute the taskB1
+        1 -> {
+            taskA1()
+            functionB(1)
+        }
+        2 -> {
+            taskA2()
+            functionB(2)
+        }
+        3 -> {
+            taskA3()
+            functionB(3)
+        }
+        4 -> {
+            taskA4()
+            functionB(4)
+        }
+    }
+}
+fun functionB(case: Int){
+    when(case){
+        1 -> {
+            taskB1()
+            functionA(1)
+        }2 -> {
+        taskB2()
+        functionA(2)
+    }3 -> {
+        taskB3()
+        functionA(3)
+    }4 -> {
+        taskB4()
+
+    }
+    }
+
+}
+fun taskA1(){println("Task A1")}
+fun taskA2(){println("Task A2")}
+fun taskA3(){println("Task A3")}
+fun taskA4(){println("Task A4")}
+fun taskB1(){println("Task B1")}
+fun taskB2(){println("Task B2")}
+fun taskB3(){println("Task B3")}
+fun taskB4(){println("Task B4")}
