3617b9d66187484a2c64384a4b55b9753cd88f70
oybekjon94 <x.oybek94@gmail.com>
2022-11-25 21:54:24 +0900

    [feat::] Add 7 exercise

---
+++ src/main/kotlin/kotlinExamples-8/7.kt
@@ -0,0 +1,24 @@
+package `kotlinExamples-8`
+
+data class Person(
+    val name: String,
+    val age: Int
+)
+
+fun findTheOldest(people: List<Person>){
+    var maxAge = 0
+    var theOldest: Person? = null
+    for (person in people) {
+        if (person.age > maxAge){
+            maxAge = person.age
+            theOldest = person
+        }
+    }
+    println(theOldest)
+}
+
+fun main(){
+    val people = listOf(Person("Oybek", 28), Person("Bob",31))
+    println(people.maxByOrNull { it.age })
+    println(people.minByOrNull { it.age })
+}
